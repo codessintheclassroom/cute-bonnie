db423ede9d9d1fcc709d9077f3327d45c7607706
Claire Beatty-Orr <beattyoc@tcd.ie>
2019-05-15 18:47:29 +0100

    Step 3

---
+++ frontend/src/Pet.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default interface Pet {
+    readonly id: string;
+    name: string;
+    //status: string
+    //kind: string;
+    //breed: string;
+    description: string;
+    //birthday: string;
+};
